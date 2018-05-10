df2eb75ccd91a55c991a80ed79bb0d0186a89dbe
Maël Brunet <maelbrunet@gmail.com>
2018-05-10 20:15:38 +0200

    Add JS pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,24 @@
-"use strict";
+'use strict';
 
-const gulp = require("gulp");
-const gulpSass = require("gulp-sass");
-const sassGlob = require("gulp-sass-glob");
-const gulpChanged = require("gulp-changed");
-const gulpImagemin = require("gulp-imagemin");
-const gulpPostcss = require("gulp-postcss");
-const autoprefixer = require("autoprefixer");
+const gulp = require('gulp');
+const gulpSass = require('gulp-sass');
+const sassGlob = require('gulp-sass-glob');
+const gulpChanged = require('gulp-changed');
+const gulpImagemin = require('gulp-imagemin');
+const gulpPostcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 const plumber = require('gulp-plumber');
-const browserSync = require("browser-sync").create();
-const del = require("del");
+const browserSync = require('browser-sync').create();
+const del = require('del');
+const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
+const uglify = require('gulp-uglify');
 
 function browsersync() {
   browserSync.init({
     server: {
-      baseDir: "./build/"
-    }
+      baseDir: './build/',
+    },
   });
 }
 
@@ -24,54 +27,71 @@ function browsersyncReload() {
 }
 
 function clean() {
-  return del(["./build/"]);
+  return del(['./build/']);
 }
 
 function copy() {
   return gulp
-    .src("./src/*")
+    .src('./src/*')
     .pipe(plumber())
-    .pipe(gulp.dest("./build"))
+    .pipe(gulp.dest('./build'))
     .pipe(browserSync.stream());
 }
 
 function css() {
   return gulp
-    .src("./src/scss/main.scss")
+    .src('./src/scss/main.scss')
     .pipe(plumber())
     .pipe(sassGlob())
     .pipe(gulpPostcss([autoprefixer()]))
     .pipe(gulpSass())
-    .pipe(gulp.dest("./build"))
+    .pipe(gulp.dest('./build'))
     .pipe(browserSync.stream());
 }
 
 function images() {
   return gulp
-    .src("./src/images/**/*")
+    .src('./src/images/**/*')
     .pipe(plumber())
-    .pipe(gulpChanged("./build/images"))
+    .pipe(gulpChanged('./build/images'))
     .pipe(
       gulpImagemin([
         gulpImagemin.jpegtran({ progressive: true }),
         gulpImagemin.optipng({ optimizationLevel: 5 }),
         gulpImagemin.svgo({
-          plugins: [{ removeViewBox: false }]
-        })
-      ])
+          plugins: [{ removeViewBox: false }],
+        }),
+      ]),
     )
-    .pipe(gulp.dest("./build/images"))
+    .pipe(gulp.dest('./build/images'))
     .pipe(browserSync.stream());
 }
 
-gulp.task("copy", copy);
-gulp.task("images", images);
-gulp.task("watch", gulp.parallel(browsersync, watchFiles));
-gulp.task("build", gulp.series(clean, gulp.parallel(copy, css, images)));
-gulp.task("default", gulp.series("build", "watch"));
+function scripts() {
+  return gulp
+    .src(['src/index.js'])
+    .pipe(plumber())
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(
+      babel({
+        presets: ['env'],
+      }),
+    )
+    .pipe(uglify())
+    .pipe(gulp.dest('build'));
+}
+
+gulp.task('copy', copy);
+gulp.task('images', images);
+gulp.task('scripts', scripts);
+gulp.task('watch', gulp.parallel(browsersync, watchFiles));
+gulp.task('build', gulp.series(clean, gulp.parallel(copy, css, scripts, images)));
+gulp.task('default', gulp.series('build', 'watch'));
 
 function watchFiles() {
-  gulp.watch(["./src/index.html"], copy);
-  gulp.watch(["./src/scss/**/*.scss"], css);
-  gulp.watch(["./src/images/*.{png,jpg,svg}"], images);
+  gulp.watch(['./src/index.html'], copy);
+  gulp.watch(['./src/scss/**/*.scss'], css);
+  gulp.watch(['./src/index.js'], scripts);
+  gulp.watch(['./src/images/*.{png,jpg,svg}'], images);
 }
